eaff9e9b2ee06461c6d4d4f8ba23953c7ec02c9b
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-11 19:10:02 -0700

    play wity dynamic classes and inline/scoped styles

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -4,26 +4,27 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    persons: [{ id:123, name: "Bob", age: 13 }, { id:2342, name: "Dos", age: 14 }],
+    persons: [
+      { id: 123, name: "Bob", age: 13 },
+      { id: 2342, name: "Dos", age: 14 },
+      { id: 23422, name: "Robert", age: 15 }
+    ],
     otherState: "Value 1"
   };
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id
-    })
+      return p.id === id;
+    });
 
-    const person = {...this.state.persons[personIndex]}
-    person.name = event.target.value
-    const persons = [...this.state.persons]
-    persons[personIndex] = person
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
     this.setState({
       persons: persons
-    })
-
-
-  }
-
+    });
+  };
 
   handleShowPersons = () => {
     this.setState({
@@ -31,23 +32,33 @@ class App extends Component {
     });
   };
 
-  deletePerson = (index) => {
-    const persons = this.state.persons.slice()
-    persons.splice(index, 1)
+  deletePerson = index => {
+    const persons = this.state.persons.slice();
+    persons.splice(index, 1);
     this.setState({
       persons: persons
-    })  
+    });
   };
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       border: "1px solid blue",
-      padding: "8px"
+      padding: "8px",
+      color: "white",
+      fontSize: "20px"
     };
+    const classes = []
+    if (this.state.persons.length <= 2){
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold')
+    }
 
     let persons = null;
 
     if (this.state.showPersons) {
+      style.backgroundColor = "red";
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
@@ -57,7 +68,7 @@ class App extends Component {
                 key={index}
                 name={person.name}
                 age={person.age}
-                changed={(event)=> this.nameChangeHandler(event, person.id)}
+                changed={event => this.nameChangeHandler(event, person.id)}
               />
             );
           })}
@@ -66,6 +77,7 @@ class App extends Component {
     }
     return (
       <div className="App">
+        <p className={classes.join(" ")}>Test TEXT </p>
         <button style={style} onClick={this.handleShowPersons}>
           Switch name
         </button>
