95ac2fe7f3b68b38015ff3fa14084b3633018435
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-16 19:43:27 -0700

    Update LIFECYCLES for props Changes

--- react-complete/src/Persons/Person/Person.js
+++ react-complete/src/Persons/Person/Person.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Person.css";
 import Radium from "radium";
 
-const person = props => {
-  const style = {
-    "@media (min-width: 500px)": {
-      width: "500px"
-    }
-  };
 
-  return (
-    <div className="Person" style={style}>
-      <h1 className="test" onClick={props.click}>
-        My name is {props.name} and I am {props.age} years old
-      </h1>
-      <h1>{props.children}</h1>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component{
+  render(){
+    const style = {
+      "@media (min-width: 500px)": {
+        width: "500px"
+      }
+    };
+    return (
+      <div className="Person" style={style}>
+        <h1 className="test" onClick={this.props.click}>
+          My name is {this.props.name} and I am {this.props.age} years old
+        </h1>
+        <h1>{this.props.children}</h1>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
 
-export default Radium(person);
+  }
+}
+
+export default Radium(Person);
--- react-complete/src/Persons/Persons.js
+++ react-complete/src/Persons/Persons.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
-const Persons = props => {
-  return props.persons.map((person, index) => {
-    return (
-        <div>
-        <p>My tiltle is {props.title}</p>
-      <Person
-        key={person.id}
-        click={() => props.clicked(index)}
-        changed={event => props.changed(event, person.id)}
-        name={person.name}
-        age={person.age}
-      />
-      </div>
-    );
-  });
-};
+
+class Persons extends Component {
+  static getDerivedStateFromProps(props, state){
+    console.log("Getting derived state ")
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+      //must return true or false  to proceed or stop updating
+      console.log("shouldComponentUpdate ")
+      return true
+  }
+
+  //legacy components below: avoid using them
+//   componentWillReceiveProps(props){
+//     console.log("componentWillReceiveProps")
+//   }
+
+  //-----------------------
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log("getSnapshotBeforeUpdate ")
+    return {message:"Snapshot!!!"}
+  }
+
+  componentDidUpdate(prevProps, prevState, snapShot){
+      //use when fetching new data from server
+      console.log("Component did update")
+      
+      console.log(snapShot)
+  }
+
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <div key={person.id}>
+          <p>My tiltle is {this.props.title}</p>
+          <Person
+            click={() => this.props.clicked(index)}
+            changed={event => this.props.changed(event, person.id)}
+            name={person.name}
+            age={person.age}
+          />
+        </div>
+      );
+    });
+  }
+}
 
 export default Persons;
