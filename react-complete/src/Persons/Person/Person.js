975da3591c2026e66ef0ff086bf277e9ec7513bb
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-17 19:13:26 -0700

    render ADJACENT JSX elements inside

--- react-complete/src/Persons/Person/Person.js
+++ react-complete/src/Persons/Person/Person.js
@@ -1,25 +1,30 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Person.css";
 import Radium from "radium";
+import Aux from "../../Aux/Aux";
 
-
-class Person extends Component{
-  render(){
+class Person extends Component {
+  render() {
     const style = {
       "@media (min-width: 500px)": {
         width: "500px"
       }
     };
     return (
-      <div className="Person" style={style}>
-        <h1 className="test" onClick={this.props.click}>
-          My name is {this.props.name} and I am {this.props.age} years old
-        </h1>
-        <h1>{this.props.children}</h1>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      <Aux>
+        <div className="Person" style={style}>
+          <h1  className="test" onClick={this.props.click}>
+            My name is {this.props.name} and I am {this.props.age} years old
+          </h1>
+          <h1>{this.props.children}</h1>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div>
+      </Aux>
     );
-
   }
 }
 
